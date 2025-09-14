d11a5c70dbbbead0460b7ea6ae8672c2ae8efa26
Charis Adiari <charis.adiari@gmail.com>
2025-09-14 21:20:11 +0200

    Added random number swapping to sudoku generation. Not confident in this code

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@ import { GameSettings } from "./utilities/game-settings.js";
 import { Board } from "./sudoku-board.js";
 import { easySeed, mediumSeed, hardSeed } from "../seed.js";
 import { Timer } from "./utilities/timer.js";
-import { SudokuGenerator } from "./utilities/sudoku-generator.js";
+import { SudokuRandomiser } from "./utilities/sudoku-randomiser.js";
 
 export class Game {
   constructor() {
@@ -45,7 +45,7 @@ export class Game {
     else if (this.level === 'medium') seed = mediumSeed[Math.floor(Math.random() * mediumSeed.length)];
     else seed = hardSeed[Math.floor(Math.random() * hardSeed.length)];
     
-    const transformedSudoku = SudokuGenerator.generateSudoku(seed.puzzle, seed.solution);
+    const transformedSudoku = SudokuRandomiser.generateSudoku(seed.puzzle, seed.solution);
     this.puzzleArray = transformedSudoku.puzzle;
     this.solutionArray = transformedSudoku.solution;
     
